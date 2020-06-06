0875a345af02ceaaa3ab1973f138abdf57f44d67
derovi <devderovi@gmail.com>
2020-06-06 14:59:22 +0300

    Added GamePlayer class

---
+++ src/main/java/by/dero/gvh/GamePlayer.java
@@ -0,0 +1,10 @@
+package by.dero.gvh;
+
+import by.dero.gvh.model.Item;
+
+import java.util.List;
+
+public class GamePlayer {
+    private String className;
+    private List<Item> items;
+}
--- src/main/java/by/dero/gvh/model/UnitClass.java
+++ src/main/java/by/dero/gvh/model/UnitClass.java
@@ -1,5 +1,24 @@
 package by.dero.gvh.model;
 
+import java.util.List;
+
 public class UnitClass {
     private String name;
+    private List<String> itemNames;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<String> getItemNames() {
+        return itemNames;
+    }
+
+    public void setItemNames(List<String> itemNames) {
+        this.itemNames = itemNames;
+    }
 }
