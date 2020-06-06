accc7c06f9b7868c3d9bd928faf02ebbac6639ac
derovi <devderovi@gmail.com>
2020-06-06 15:32:15 +0300

    AddItem method added

--- src/main/java/by/dero/gvh/GamePlayer.java
+++ src/main/java/by/dero/gvh/GamePlayer.java
@@ -1,7 +1,10 @@
 package by.dero.gvh;
 
 import by.dero.gvh.model.Item;
+import by.dero.gvh.model.ItemDescription;
+import by.dero.gvh.model.ItemInfo;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.ItemStack;
 
 import java.util.List;
 
@@ -14,6 +17,20 @@ public class GamePlayer {
     public GamePlayer(Player player, String className) {
         this.player = player;
         this.className = className;
+    }
+
+    public void addItem(String name, int level) {
+        try {
+            ItemDescription itemDescription = Plugin.getInstance().getData().getItems().get(name);
+            ItemInfo itemInfo = itemDescription.getLevels().get(level);
+            Item item = (Item) Plugin.getInstance().getData().getItemNameToClass().
+                    get(name).getConstructor().newInstance();
+            items.add(item);
+            ItemStack itemStack = new ItemStack(itemInfo.getMaterial(), itemInfo.getAmount());
+            player.getInventory().setItem(itemDescription.getSlot(), itemStack);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     public Player getPlayer() {
