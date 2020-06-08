0756d8cae2ba57ab2643ed7ea81c1e1d77624a08
derovi <devderovi@gmail.com>
2020-06-08 13:07:42 +0300

    Added gson library

--- src/main/java/by/dero/gvh/model/ItemInfo.java
+++ src/main/java/by/dero/gvh/model/ItemInfo.java
@@ -2,13 +2,14 @@ package by.dero.gvh.model;
 
 import org.bukkit.Material;
 
+import java.util.Arrays;
 import java.util.List;
 
 public class ItemInfo {
-    private Material material;
-    private String displayName;
-    private List<String> lore;
-    private int amount;
+    private Material material = Material.BEDROCK;
+    private String displayName = "§cNot found";
+    private List<String> lore = Arrays.asList("First line", "Second line");
+    private int amount = 1;
 
     public Material getMaterial() {
         return material;
