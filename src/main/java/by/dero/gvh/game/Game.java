f4feecaa44d10b7e64d59be9666a49231a3a127a
derovi <devderovi@gmail.com>
2020-06-14 19:29:22 +0300

    Finish command fix

--- src/main/java/by/dero/gvh/game/Game.java
+++ src/main/java/by/dero/gvh/game/Game.java
@@ -24,6 +24,7 @@ public abstract class Game implements Listener {
     private final HashMap<String, GamePlayer> players = new HashMap<>();
 
     public void start() {
+        System.out.println("start game");
         if (state == State.GAME) {
             System.err.println("Can't start game, already started!");
             return;
@@ -33,9 +34,11 @@ public abstract class Game implements Listener {
             return;
         }
         chooseTeams();
+        System.out.println("starting");
         for (GamePlayer player : players.values()) {
             spawnPlayer(player, 0);
         }
+        System.out.println("spawned");
         state = State.GAME;
         lobby = null;
     }
@@ -120,7 +123,7 @@ public abstract class Game implements Listener {
         System.out.println("class: " + player.getClassName());
         System.out.println("null: " + (classDescription == null));
         for (String itemName : classDescription.getItemNames()) {
-            player.addItem(itemName, 0);
+            player.addItem(itemName, player.getPlayerInfo().getItemLevel(player.getClassName(), itemName));
         }
     }
 
