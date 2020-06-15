5761cd6e96c6e0d2747122846656439461ffe058
derovi <devderovi@gmail.com>
2020-06-15 19:35:00 +0300

    Added settings

--- src/main/java/by/dero/gvh/Settings.java
+++ src/main/java/by/dero/gvh/Settings.java
@@ -1,4 +1,67 @@
 package by.dero.gvh;
 
 public class Settings {
+    private String mode;
+    private String dataStorageType;
+    private String dataMongodbConnection;
+    private String dataMongodbDatabase;
+    private String playerDataStorageType;
+    private String playerDataMongodbConnection;
+    private String playerDataMongodbDatabase;
+
+    public String getMode() {
+        return mode;
+    }
+
+    public void setMode(String mode) {
+        this.mode = mode;
+    }
+
+    public String getDataStorageType() {
+        return dataStorageType;
+    }
+
+    public void setDataStorageType(String dataStorageType) {
+        this.dataStorageType = dataStorageType;
+    }
+
+    public String getDataMongodbConnection() {
+        return dataMongodbConnection;
+    }
+
+    public void setDataMongodbConnection(String dataMongodbConnection) {
+        this.dataMongodbConnection = dataMongodbConnection;
+    }
+
+    public String getDataMongodbDatabase() {
+        return dataMongodbDatabase;
+    }
+
+    public void setDataMongodbDatabase(String dataMongodbDatabase) {
+        this.dataMongodbDatabase = dataMongodbDatabase;
+    }
+
+    public String getPlayerDataStorageType() {
+        return playerDataStorageType;
+    }
+
+    public void setPlayerDataStorageType(String playerDataStorageType) {
+        this.playerDataStorageType = playerDataStorageType;
+    }
+
+    public String getPlayerDataMongodbConnection() {
+        return playerDataMongodbConnection;
+    }
+
+    public void setPlayerDataMongodbConnection(String playerDataMongodbConnection) {
+        this.playerDataMongodbConnection = playerDataMongodbConnection;
+    }
+
+    public String getPlayerDataMongodbDatabase() {
+        return playerDataMongodbDatabase;
+    }
+
+    public void setPlayerDataMongodbDatabase(String playerDataMongodbDatabase) {
+        this.playerDataMongodbDatabase = playerDataMongodbDatabase;
+    }
 }
