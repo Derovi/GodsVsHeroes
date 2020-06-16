d232ae94d533628e7d5919ab2d65df2a267b3e59
derovi <devderovi@gmail.com>
2020-06-16 14:38:40 +0300

    Added default world generation

--- src/main/java/by/dero/gvh/lobby/Lobby.java
+++ src/main/java/by/dero/gvh/lobby/Lobby.java
@@ -2,12 +2,36 @@ package by.dero.gvh.lobby;
 
 import by.dero.gvh.Plugin;
 import by.dero.gvh.PluginMode;
+import by.dero.gvh.lobby.utils.VoidGenerator;
+import by.dero.gvh.model.storages.LocalStorage;
+import by.dero.gvh.utils.DataUtils;
+import by.dero.gvh.utils.ResourceUtils;
+import com.google.gson.Gson;
 import org.bukkit.Bukkit;
+import org.bukkit.World;
+import org.bukkit.WorldCreator;
 
 public class Lobby implements PluginMode {
+    private LobbyInfo info;
+    private final String worldName = "Lobby";
+    private World world;
+
     @Override
     public void onEnable() {
         registerEvents();
+        try {
+            info = new Gson().fromJson(DataUtils.loadOrDefault(new LocalStorage(), "lobby", "lobby",
+                    ResourceUtils.readResourceFile("/lobby/lobby.json")), LobbyInfo.class);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+        // create world
+        if (Plugin.getInstance().getServer().getWorld(worldName) == null) {
+            WorldCreator creator = new WorldCreator(worldName);
+            creator.generator(new VoidGenerator());
+            world = creator.createWorld();
+        }
+        world = Plugin.getInstance().getServer().getWorld(worldName);
     }
 
     @Override
@@ -15,5 +39,17 @@ public class Lobby implements PluginMode {
 
     private void registerEvents() {
         Bukkit.getPluginManager().registerEvents(new LobbyEvents(), Plugin.getInstance());
+    }
+
+    public LobbyInfo getInfo() {
+        return info;
+    }
+
+    public String getWorldName() {
+        return worldName;
+    }
+
+    public World getWorld() {
+        return world;
     }
 }
--- src/main/java/by/dero/gvh/lobby/LobbyData.java
+++ src/main/java/by/dero/gvh/lobby/LobbyData.java
@@ -1,12 +1,53 @@
 package by.dero.gvh.lobby;
 
+import by.dero.gvh.minigame.Position;
 import by.dero.gvh.model.StorageInterface;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
 
 public class LobbyData {
-    private StorageInterface storage;
+    private final StorageInterface storage;
 
     public LobbyData(StorageInterface storage) {
         this.storage = storage;
+    }
+
+    public Position getLastLobbyPosition() {
+        String json = storage.load("lobby", "lastPosition");
+        if (json == null) {
+            return new Position(0,64,0);
+        }
+        return new Gson().fromJson(json, Position.class);
+    }
+
+    public void updateLastLobbyPosition(Position position) {
+        try {
+            storage.save("lobby", "lastPosition",
+                    new GsonBuilder().setPrettyPrinting().create().toJson(position));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public LobbyRecord getRecord(String playerName) {
+        String recordJson = storage.load("lobbyRecords", playerName);
+        if (recordJson == null) {
+            return null;
+        }
+        return new Gson().fromJson(recordJson, LobbyRecord.class);
+    }
+
+    public boolean isRecordExists(String playerName) {
+        return storage.exists("lobbyRecords", playerName);
+    }
+
+    public void saveRecord(String playerName, LobbyRecord record) {
+        try {
+            storage.save("lobbyRecords", playerName,
+                    new GsonBuilder().setPrettyPrinting().create().toJson(record));
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
     }
 
     public StorageInterface getStorage() {
