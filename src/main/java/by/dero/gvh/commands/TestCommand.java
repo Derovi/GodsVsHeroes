47b510b1f89a9c6e6873201ba2b176167adcf792
derovi <devderovi@gmail.com>
2020-06-27 15:06:02 +0300

    Added gravity fireball

--- src/main/java/by/dero/gvh/commands/TestCommand.java
+++ src/main/java/by/dero/gvh/commands/TestCommand.java
@@ -1,30 +1,39 @@
 package by.dero.gvh.commands;
 
+import by.dero.gvh.nmcapi.throwing.GravityFireball;
 import by.dero.gvh.nmcapi.throwing.ThrowingAxe;
 import by.dero.gvh.nmcapi.throwing.ThrowingItem;
 import by.dero.gvh.nmcapi.throwing.ThrowingSword;
+import net.minecraft.server.v1_12_R1.PacketPlayOutEntityDestroy;
 import net.minecraft.server.v1_12_R1.Vector3f;
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.EnderDragon;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.Player;
+import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftSnowball;
+import org.bukkit.entity.*;
 import org.bukkit.util.Vector;
 
 public class TestCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender,
                              Command command, String s, String[] strings) {
+
         Player player = (Player) commandSender;
-        //Location loc = player.getLocation().clone();
-        //loc.setX(loc.getX() + 20);
-        //EnderDragon enderDragon = (EnderDragon) player.getWorld().spawnEntity(loc, EntityType.ENDER_DRAGON);
-        //enderDragon.addPassenger(player);
-        ThrowingSword axe = new ThrowingSword(player, player.getItemInHand().getType());
-       axe.spawn();
+        GravityFireball gravityFireball = new GravityFireball(player.getEyeLocation().add(new Vector(0,0.5,0)));
+        Vector direction = player.getLocation().getDirection().normalize();
+        //gravityFireball.setDirection(direction.getX(), direction.getY(), direction.getZ());
+        gravityFireball.spawn();
+        gravityFireball.setVelocity(player.getLocation().getDirection());
+        gravityFireball.getFireball().addPassenger(player);
+        System.out.println("spawned");
+        //Fireball fireball = ((Fireball) gravityFireball.bukkitEntity);
+        //fireball.setVelocity(player.getLocation().getDirection().normalize());
+        //fireball.addPassenger(player);
         return true;
     }
 }
