e57926461d1923a2459f3e9c7a1bbb91c2b2c1e5
derovi <dirbraindrv@gmail.com>
2020-07-02 00:29:42 +0300

    Fixed conflicts

--- src/main/java/by/dero/gvh/commands/TestCommand.java
+++ src/main/java/by/dero/gvh/commands/TestCommand.java
@@ -1,31 +1,15 @@
 package by.dero.gvh.commands;
 
 import by.dero.gvh.Plugin;
-import by.dero.gvh.nmcapi.InstantFirework;
-<<<<<<< HEAD
 import by.dero.gvh.nmcapi.PassiveChicken;
-=======
-import by.dero.gvh.nmcapi.SmartFallingBlock;
->>>>>>> 26677aecc494980f88cf974caf239fa74d804de9
-import by.dero.gvh.nmcapi.throwing.GravityFireball;
 import net.minecraft.server.v1_12_R1.EntityPlayer;
 import net.minecraft.server.v1_12_R1.PacketPlayOutCamera;
-import org.bukkit.*;
-import org.bukkit.block.Block;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.craftbukkit.v1_12_R1.entity.CraftChicken;
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
 import org.bukkit.entity.*;
-import org.bukkit.inventory.meta.FireworkMeta;
-<<<<<<< HEAD
 import org.bukkit.scheduler.BukkitRunnable;
-=======
->>>>>>> 26677aecc494980f88cf974caf239fa74d804de9
-import org.bukkit.util.Vector;
-
-import static by.dero.gvh.model.Drawings.spawnFirework;
 
 public class TestCommand implements CommandExecutor {
     @Override
