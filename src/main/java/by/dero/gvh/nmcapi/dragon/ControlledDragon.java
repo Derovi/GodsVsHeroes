1c026c8f17b4450499873076e9420af81b831f11
derovi <devderovi@gmail.com>
2020-07-02 18:40:59 +0300

    Added dragon fly

--- src/main/java/by/dero/gvh/nmcapi/dragon/ControlledDragon.java
+++ src/main/java/by/dero/gvh/nmcapi/dragon/ControlledDragon.java
@@ -2,6 +2,7 @@ package by.dero.gvh.nmcapi.dragon;
 
 import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
+import org.bukkit.util.Vector;
 
 public class ControlledDragon {
     private DragonBase dragonBase;
@@ -14,10 +15,10 @@ public class ControlledDragon {
         this.player = player;
         dragonBase = new DragonBase(this);
         dragonBase.spawn();
-        RotatingDragon dragon = new RotatingDragon(player.getLocation());
+        dragon = new RotatingDragon(player.getLocation());
         dragon.spawn();
-        EmptyArmorStand empty1 = new EmptyArmorStand(player.getLocation());
-        EmptyArmorStand empty2 = new EmptyArmorStand(player.getLocation());
+        empty1 = new EmptyArmorStand(player.getLocation());
+        empty2 = new EmptyArmorStand(player.getLocation());
         empty1.spawn();
         empty2.spawn();
         dragon.a(dragonBase, true);
@@ -28,7 +29,24 @@ public class ControlledDragon {
 
     // called from DragonBase.B_()
     public void update() {
+        float pitch = player.getLocation().getPitch();
+        if (player.getLocation().getPitch() < 0.0f) {
+            pitch = 0.0f;
+        }
+        else if (player.getLocation().getPitch() > 60.0f) {
+            pitch = 60.0f;
+        }
+        final Vector vec = player.getLocation().getDirection().multiply(1).setY(pitch / 40.0f * -1.0f + 0.75);
+        move(vec.getX(), vec.getY(), vec.getZ());
 
+        dragon.yaw = player.getLocation().getYaw() - 180.0f;
+        empty2.yaw = player.getLocation().getYaw();
+    }
+
+    public void move(double dx, double dy, double dz) {
+        dragonBase.locX += dx;
+        dragonBase.locY += dy;
+        dragonBase.locZ += dz;
     }
 
     public Player getPlayer() {
