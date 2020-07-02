10190e395b014eea8f3c821a964f902e122c8605
derovi <devderovi@gmail.com>
2020-07-02 18:31:19 +0300

    Added controlled dragon

--- src/main/java/by/dero/gvh/commands/TestCommand.java
+++ src/main/java/by/dero/gvh/commands/TestCommand.java
@@ -1,6 +1,7 @@
 package by.dero.gvh.commands;
 
 import by.dero.gvh.Plugin;
+import by.dero.gvh.nmcapi.dragon.ControlledDragon;
 import by.dero.gvh.nmcapi.dragon.RotatingDragon;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -14,27 +15,7 @@ public class TestCommand implements CommandExecutor {
     public boolean onCommand(CommandSender commandSender,
                              Command command, String s, String[] strings) {
         Player player = (Player) commandSender;
-        RotatingDragon dragon = new RotatingDragon(player.getLocation());
-        dragon.spawn();
-        System.out.println("Duration " + 400);
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-            }
-        }.runTaskLater(Plugin.getInstance(), 400);
-        dragon.getBukkitEntity().setVelocity(new Vector(0,1,0));
-        new BukkitRunnable() {
-            @Override
-            public void run() {
-            }
-        }.runTaskLater(Plugin.getInstance(), 25);
-        ArmorStand armorStand = (ArmorStand) player.getWorld().spawnEntity(player.getLocation(), EntityType.ARMOR_STAND);
-        ArmorStand armorStand2 = (ArmorStand) player.getWorld().spawnEntity(player.getLocation(), EntityType.ARMOR_STAND);
-        ArmorStand armorStand3 = (ArmorStand) player.getWorld().spawnEntity(player.getLocation(), EntityType.ARMOR_STAND);
-        armorStand.addPassenger(dragon.getBukkitEntity());
-        armorStand.addPassenger(armorStand2);
-        armorStand2.addPassenger(armorStand3);
-        armorStand3.addPassenger(player);
+        ControlledDragon controlledDragon = new ControlledDragon(player);
         return true;
     }
 }
--- src/main/java/by/dero/gvh/nmcapi/dragon/ControlledDragon.java
+++ src/main/java/by/dero/gvh/nmcapi/dragon/ControlledDragon.java
@@ -1,12 +1,29 @@
 package by.dero.gvh.nmcapi.dragon;
 
+import org.bukkit.craftbukkit.v1_12_R1.entity.CraftPlayer;
 import org.bukkit.entity.Player;
 
 public class ControlledDragon {
+    private DragonBase dragonBase;
+    private RotatingDragon dragon;
+    private EmptyArmorStand empty1;
+    private EmptyArmorStand empty2;
     private Player player;
 
     public ControlledDragon(Player player) {
         this.player = player;
+        dragonBase = new DragonBase(this);
+        dragonBase.spawn();
+        RotatingDragon dragon = new RotatingDragon(player.getLocation());
+        dragon.spawn();
+        EmptyArmorStand empty1 = new EmptyArmorStand(player.getLocation());
+        EmptyArmorStand empty2 = new EmptyArmorStand(player.getLocation());
+        empty1.spawn();
+        empty2.spawn();
+        dragon.a(dragonBase, true);
+        empty1.a(dragonBase, true);
+        empty2.a(empty1, true);
+        ((CraftPlayer) player).getHandle().a(empty2, true);
     }
 
     // called from DragonBase.B_()
