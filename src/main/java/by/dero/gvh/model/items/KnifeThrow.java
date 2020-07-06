4ebf483a4c364d8ad6dd64d657b92ef44b6499e1
Hattonuri <stadima35@gmail.com>
2020-07-06 23:45:03 +0300

    fixes

--- src/main/java/by/dero/gvh/model/items/KnifeThrow.java
+++ src/main/java/by/dero/gvh/model/items/KnifeThrow.java
@@ -3,7 +3,6 @@ package by.dero.gvh.model.items;
 import by.dero.gvh.Plugin;
 import by.dero.gvh.minigame.Game;
 import by.dero.gvh.model.Item;
-import by.dero.gvh.model.interfaces.InfiniteReplenishInterface;
 import by.dero.gvh.model.interfaces.PlayerInteractInterface;
 import by.dero.gvh.model.itemsinfo.KnifeThrowInfo;
 import by.dero.gvh.nmcapi.throwing.ThrowingKnife;
@@ -18,7 +17,7 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.material.MaterialData;
 import org.bukkit.scheduler.BukkitRunnable;
 
-public class KnifeThrow extends Item implements PlayerInteractInterface, InfiniteReplenishInterface {
+public class KnifeThrow extends Item implements PlayerInteractInterface {
     private final KnifeThrowInfo info;
 
     public KnifeThrow(String name, int level, Player owner) {
@@ -28,6 +27,10 @@ public class KnifeThrow extends Item implements PlayerInteractInterface, Infinit
 
     @Override
     public void onPlayerInteract(PlayerInteractEvent event) {
+        if (cooldown.isReady()) {
+            return;
+        }
+        cooldown.reload();
         final int slot = owner.getInventory().getHeldItemSlot();
         final ThrowingKnife knife = new ThrowingKnife(owner, info.getMaterial());
         owner.getWorld().playSound(owner.getLocation(), Sound.BLOCK_CLOTH_STEP,  24, 1);
