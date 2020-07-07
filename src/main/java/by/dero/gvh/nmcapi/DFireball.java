fe55cc4b10061014d1e0555ef66ee8a4cc676722
Hattonuri <stadima35@gmail.com>
2020-07-07 18:18:48 +0300

    added explosion

--- src/main/java/by/dero/gvh/nmcapi/DFireball.java
+++ src/main/java/by/dero/gvh/nmcapi/DFireball.java
@@ -1,10 +1,14 @@
 package by.dero.gvh.nmcapi;
 
 import by.dero.gvh.GamePlayer;
-import net.minecraft.server.v1_12_R1.*;
+import by.dero.gvh.utils.GameUtils;
+import net.minecraft.server.v1_12_R1.EntityDragonFireball;
+import net.minecraft.server.v1_12_R1.MovingObjectPosition;
 import org.bukkit.Location;
+import org.bukkit.Particle;
+import org.bukkit.Sound;
 import org.bukkit.craftbukkit.v1_12_R1.CraftWorld;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 
 public class DFireball extends EntityDragonFireball {
@@ -19,7 +23,14 @@ public class DFireball extends EntityDragonFireball {
     public void a(MovingObjectPosition var1) {
         if (var1.entity == null || !var1.entity.s(this.shooter)) {
             if (!this.world.isClientSide) {
-                // EXPLODE
+                final Location loc = new Location(owner.getPlayer().getWorld(), locX, locY, locZ);
+                loc.getWorld().spawnParticle(Particle.EXPLOSION_HUGE, loc, 1);
+                loc.getWorld().playSound(loc, Sound.ENTITY_GENERIC_EXPLODE, 1.07f, 1);
+                for (final LivingEntity ent : GameUtils.getNearby(loc, 3)) {
+                    if (GameUtils.isEnemy(ent, owner.getPlayer())) {
+                        GameUtils.damage(10, ent, owner.getPlayer());
+                    }
+                }
                 this.die();
             }
         }
