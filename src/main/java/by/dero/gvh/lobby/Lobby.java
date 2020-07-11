d9bbfd01f5aa824685518f11aaf9c0238d9f2daf
Hattonuri <stadima35@gmail.com>
2020-07-11 14:11:31 +0300

    Removed block damage

--- src/main/java/by/dero/gvh/lobby/Lobby.java
+++ src/main/java/by/dero/gvh/lobby/Lobby.java
@@ -23,6 +23,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.FoodLevelChangeEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -299,6 +300,11 @@ public class Lobby implements PluginMode, Listener {
             p.setVelocity(p.getVelocity().add(new Vector(0,1,0)));
             event.setCancelled (true);
         }
+    }
+
+    @EventHandler
+    public void onBlockPlace(BlockPlaceEvent event) {
+        event.setCancelled(true);
     }
 
     @EventHandler
