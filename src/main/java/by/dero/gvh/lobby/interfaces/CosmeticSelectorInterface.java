65ce005186716b768f4a95e206aa61a954973c3f
derovi <devderovi@gmail.com>
2020-07-23 19:59:30 +0300

    Interfaces connected

--- src/main/java/by/dero/gvh/lobby/interfaces/CosmeticSelectorInterface.java
+++ src/main/java/by/dero/gvh/lobby/interfaces/CosmeticSelectorInterface.java
@@ -1,6 +1,9 @@
 package by.dero.gvh.lobby.interfaces;
 
 import by.dero.gvh.Plugin;
+import by.dero.gvh.lobby.Lobby;
+import by.dero.gvh.lobby.interfaces.cosmetic.AllCosmetic;
+import by.dero.gvh.lobby.interfaces.cosmetic.CosmeticInterfaces;
 import by.dero.gvh.minigame.Heads;
 import by.dero.gvh.model.Lang;
 import by.dero.gvh.model.UnitClassDescription;
@@ -24,18 +27,31 @@ public class CosmeticSelectorInterface extends Interface {
 		InterfaceUtils.changeName(emptySlot, Lang.get("interfaces.empty"));
 		ItemStack common = GameUtils.getHead(player);
 		InterfaceUtils.changeName(common, Lang.get("cosmetic.common"));
-		
-		ArrayList<ItemStack> classSlots = new ArrayList<>(classes.size());
+
+
+		addButton(0, getHeight() - 1, common, () -> {
+			CosmeticInterface interfaceObject = new AllCosmetic(Lobby.getInstance().getInterfaceManager(),
+					getPlayer(), "all");
+			interfaceObject.open();
+		});
+		int x = 2, y = getHeight() - 1;
 		for (UnitClassDescription desc : classes) {
 			ItemStack head = Heads.getHead(desc.getName());
 			InterfaceUtils.changeName(head, "§a" + Lang.get("classes." + desc.getName()));
-			classSlots.add(head);
-		}
-		
-		addItem(0, getHeight() - 1, common);
-		int x = 2, y = getHeight() - 1;
-		for (ItemStack slot : classSlots) {
-			addItem(x, y, slot);
+			if (!CosmeticInterfaces.exists(desc.getName())) {
+				InterfaceUtils.changeName(head, Lang.get("interfaces.soon"));
+				addItem(x, y, head);
+			} else addButton(x, y, head, () -> {
+				try {
+					Class<? extends CosmeticInterface> interfaceClass = CosmeticInterfaces.get(desc.getName());
+					CosmeticInterface interfaceObject = interfaceClass.getConstructor(InterfaceManager.class,
+							Player.class, String.class).newInstance(Lobby.getInstance().getInterfaceManager(),
+							getPlayer(), desc.getName());
+					interfaceObject.open();
+				} catch (Exception ex) {
+					ex.printStackTrace();
+				}
+			});
 			x++;
 			if (x == 9) {
 				x = 2;
---
+++ src/main/java/by/dero/gvh/lobby/interfaces/cosmetic/CosmeticInterfaces.java
@@ -0,0 +1,21 @@
+package by.dero.gvh.lobby.interfaces.cosmetic;
+
+import by.dero.gvh.lobby.interfaces.CosmeticInterface;
+
+import java.util.HashMap;
+
+public class CosmeticInterfaces {
+    private static final HashMap<String, Class<? extends CosmeticInterface>> interfaces = new HashMap<>();
+
+    public static boolean exists(String name) {
+        return interfaces.containsKey(name);
+    }
+
+    public static Class<? extends CosmeticInterface> get(String name) {
+        return interfaces.get(name);
+    }
+
+    static {
+        interfaces.put("horseman", AllCosmetic.class);
+    }
+}
