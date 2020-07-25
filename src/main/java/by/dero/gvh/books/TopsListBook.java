5d8c80ad9a9cc6384ec248da5261aaf66d38ccef
derovi <devderovi@gmail.com>
2020-07-25 17:49:07 +0300

    Fixed

--- src/main/java/by/dero/gvh/books/TopsListBook.java
+++ src/main/java/by/dero/gvh/books/TopsListBook.java
@@ -9,11 +9,11 @@ import by.dero.gvh.stats.GamePlayerStats;
 import by.dero.gvh.stats.GameStats;
 import org.bukkit.entity.Player;
 
-public class GameStatsBook extends BookGUI {
+public class TopsListBook extends BookGUI {
     private GameStats game;
     private Player considered;
 
-    public GameStatsBook(BookManager manager, Player player, Player considered, GameStats game) {
+    public TopsListBook(BookManager manager, Player player, Player considered, GameStats game) {
         super(manager, player);
         this.game = game;
         this.considered = considered;
@@ -22,33 +22,29 @@ public class GameStatsBook extends BookGUI {
     @Override
     public void build() {
         GamePlayerStats playerStats = game.getPlayers().get(considered.getName());
-        String winStatus = "§a§lПобеда";
-        if (playerStats.getTeam() != game.getWonTeam()) {
-            winStatus = "§c§lПоражение";
-        }
         BookUtil.PageBuilder builder = new BookUtil.PageBuilder()
-                .add(winStatus).add("§8 - Игра #" + game.getId()).newLine().newLine();
+                .add("§6§lТопы§8 - Игра #" + game.getId()).newLine().newLine();
         if (considered.getUniqueId().equals(getPlayer().getUniqueId())) {
             builder.add("§6Ваша статистика:");
         } else {
             builder.add("§6Статистика §8»§9 " + considered.getName() + "§6:");
         }
         builder.newLine();
-        builder.add("§0Класс §8» §5" + Lang.get("classes." + playerStats.getClassName())).newLine();
-        builder.add("§0Убийств §8» §a" + playerStats.getKills()).newLine();
-        builder.add("§0Смертей §8» §c" + playerStats.getDeaths()).newLine();
-        builder.add("§0Помощей §8» §6" + playerStats.getAssists()).newLine();
-        builder.add("§0Захват §8» §9" + playerStats.getCapturePoints()).newLine();
-        builder.add("§0Урон §8» §4" + playerStats.getDamageDealt()).newLine();
-        builder.add("§0Опыт §8» §2" + playerStats.getExpGained()).newLine();
+        builder.add("§0Класс §8>> §5" + Lang.get("classes." + playerStats.getClassName())).newLine();
+        builder.add("§0Убийств §8>> §a" + playerStats.getKills()).newLine();
+        builder.add("§0Смертей §8>> §c" + playerStats.getDeaths()).newLine();
+        builder.add("§0Помощей §8>> §6" + playerStats.getAssists()).newLine();
+        builder.add("§0Захват §8>> §9" + playerStats.getCapturePoints()).newLine();
+        builder.add("§0Урон §8>> §4" + playerStats.getDamageDealt()).newLine();
+        builder.add("§0Опыт §8>> §2" + playerStats.getExpGained()).newLine();
 
-        /*builder.newLine();
-        builder.add(BookUtil.TextBuilder.of("§5§l[Топы]").onClick(new BookButton(this, new Runnable() {
+        builder.newLine();
+        builder.add(BookUtil.TextBuilder.of("§5§l[Назад]").onClick(new BookButton(this, new Runnable() {
             @Override
             public void run() {
 
             }
-        })).build());*/
+        })).build());
 
         setBook(BookUtil.writtenBook()
                 .author("§6derovi")
