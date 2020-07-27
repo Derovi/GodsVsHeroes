3ef8f5fd4a68c06e0c1e09c5a9793f59cbd61e2a
Hattonuri <stadima35@gmail.com>
2020-07-27 19:22:12 +0300

    Multipliers

--- src/main/java/by/dero/gvh/minigame/ethercapture/EtherCapture.java
+++ src/main/java/by/dero/gvh/minigame/ethercapture/EtherCapture.java
@@ -29,6 +29,7 @@ import ru.cristalix.core.formatting.Colors;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 
 import static by.dero.gvh.model.Drawings.spawnFireworks;
 
@@ -74,6 +75,8 @@ public class EtherCapture extends Game implements DisplayInteractInterface {
         }
     }
 
+    
+    private final HashMap<GamePlayer, Double> boosterMult = new HashMap<>();
     @Override
     public void updateDisplays() {
         int cnt = getInfo().getTeamCount();
@@ -105,7 +108,14 @@ public class EtherCapture extends Game implements DisplayInteractInterface {
             str[cnt] = Lang.get("commands.playingFor").
                     replace("%com%", Lang.get("commands." + (char)('1' + gp.getTeam())));
 //            str[cnt+2] = Lang.get("game.classSelected").replace("%class%", Lang.get("classes." + gp.getClassName()));
-            str[cnt+3] = Lang.get("game.expGained").replace("%exp%", String.valueOf(stats.getExpGained()));
+    
+            double mult = boosterMult.getOrDefault(gp, -1.0);
+            if (mult == -1) {
+                mult = Plugin.getInstance().getBoosterManager().calculateMultiplier(this, gp);
+                boosterMult.put(gp, mult);
+            }
+            str[cnt+3] = (mult == 1 ? Lang.get("game.expGained") : Lang.get("game.expGainedMult").
+                    replace("%mul%", String.format("%.2f", mult))).replace("%exp%", String.valueOf(stats.getExpGained() * mult));
             str[cnt+4] = Lang.get("game.kills").replace("%kills%", String.valueOf(stats.getKills()));
             str[cnt+5] = Lang.get("game.deaths").replace("%deaths%", String.valueOf(stats.getDeaths()));
             str[cnt+6] = Lang.get("game.assists").replace("%assists%", String.valueOf(stats.getAssists()));
